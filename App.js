ed94f07aacf6c58a8303a19d036d3a0f64c6d00e
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-07-31 23:15:42 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//setup redux
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lets build Uber11</Text>
       <StatusBar style="auto" />
     </View>
   );
