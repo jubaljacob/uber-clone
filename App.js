619451286afe34f4baeb7dc18d7a4747fdb1a523
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-01 02:55:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './slices/screens/HomeScreen';
 import { store } from './store';
 
 //setup redux
@@ -9,9 +10,7 @@ import { store } from './store';
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build Uber!</Text>
-      </View>
+      <HomeScreen/>
     </Provider>
 
   );
