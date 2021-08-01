11a697a7a1eef79f0fc16a691e8b8729d99fc1ed
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-01 13:27:01 +0530

    Create MapScreen.js

---
+++ screens/MapScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text>Here is The map stuff</Text>
+        </View>
+    );
+};
+export default MapScreen;
+
+const styles = StyleSheet.create({});
