247140661f1032c0865f42aa11048397ca31677f
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-01 13:02:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './slices/screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 //setup redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen/>
+      <SafeAreaProvider>
+        <HomeScreen/>
+      </SafeAreaProvider>
+      
     </Provider>
 
   );
